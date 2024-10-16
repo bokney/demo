bb365aa779750398bbe60d5080c6bc7ba4d140b7
bokney <tokny@live.com>
2024-10-16 02:57:44 +0100

    mocking GBDK

---
+++ test/mock_crash_handler.h
@@ -0,0 +1,2 @@
+
+void __HandleCrash(void);
---
+++ test/mock_gb.h
@@ -0,0 +1,9 @@
+
+#ifndef MOCK_GB_H
+#define MOCK_GB_H
+
+typedef unsigned char uint8_t;
+typedef unsigned short uint16_t;
+typedef unsigned int uint32_t;
+
+#endif
