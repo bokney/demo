51b31bcc10db09d6aaacf9e56dc2d3a5ffcb7097
bokney <tokny@live.com>
2024-10-16 21:56:51 +0100

    a somewhat valid test now exists

--- test/test_state_manager.c
+++ test/test_state_manager.c
@@ -2,29 +2,67 @@
 #include <unity.h>
 #include "state_manager.h"
 
-int add(int a, int b) {
-    return a + b;
-}
+uint8_t test_counter_a, test_counter_b;
 
 void setUp(void) {
-
+    // test_counter_a = 10;
+    // test_counter_b = 10;
 }
 
 void tearDown(void) {
 
 }
 
-void test_addition(void) {
-    TEST_ASSERT_EQUAL(5, add(2, 3));
+/*
+    things to test...
+    - init and first iter trigger same invocation of runStep
+    - last iter and exit trigger same invocation of runStep
+    - correct amount of iter iterations
+*/
+
+void test_init(void) {
+    test_counter_a = 4;
+    test_counter_b = 0;
+    printf("WHAT?\n");
 }
 
-void test_a(void) {
+uint8_t test_iter(void) {
+    test_counter_a--;
+    if (test_counter_a == 0)
+        return 1;
+    return 0;
+}
+
+uint8_t test_exit(void) {
+    test_counter_b++;
+    return 0;
+}
+
+void test_init_and_first_iter_trigger_on_same_runstep_invocation(void) {
     state test_state;
     test_state.step = 0;
+    test_state.init = &test_init;
+    test_state.iter = &test_iter;
+    test_state.exit = NULL;
+    runState(&test_state);
+    TEST_ASSERT_EQUAL(test_counter_a, 3);
+    TEST_ASSERT_EQUAL(test_counter_b, 0);
+    runState(&test_state);
+    TEST_ASSERT_EQUAL(test_counter_a, 2);
+    TEST_ASSERT_EQUAL(test_counter_b, 0);
+    runState(&test_state);
+    TEST_ASSERT_EQUAL(test_counter_a, 1);
+    TEST_ASSERT_EQUAL(test_counter_b, 0);
+    runState(&test_state);
+    TEST_ASSERT_EQUAL(test_counter_a, 0);
+    TEST_ASSERT_EQUAL(test_counter_b, 0);
+    runState(&test_state);
+    TEST_ASSERT_EQUAL(test_counter_a, 3);
+    TEST_ASSERT_EQUAL(test_counter_b, 0);
 }
 
 int main(void) {
     UNITY_BEGIN();
-    RUN_TEST(test_addition);
+    RUN_TEST(test_init_and_first_iter_trigger_on_same_runstep_invocation);
     return UNITY_END();
 }
