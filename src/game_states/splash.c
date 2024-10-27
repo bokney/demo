68d7750bf439ef41d2d8d19595d9e6979cc25611
bokney <tokny@live.com>
2024-10-27 17:56:54 +0000

    game states updated

--- src/game_states/splash.c
+++ src/game_states/splash.c
@@ -10,13 +10,19 @@ splashData splash_data;
 
 void splash_init(void) {
     splash_data.count = 0;
+    printf("Splash Init\n");
 }
 
 uint8_t splash_iter(void) {
+    printf("Splash Iter %i\n", splash_data.count);
     splash_data.count += 1;
+    if (splash_data.count > 8)
+        return 1;
     return 0;
 }
 
 uint8_t splash_exit(void) {
-    return 1;
+    printf("Splash Exit\n");
+    return 0;
 }
+
